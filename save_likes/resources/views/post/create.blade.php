e92e54b745fd7133e9367d4eecee0ace4bdeca7d
akadoh4869 <hodakobe@gmail.com>
2023-11-25 12:58:22 +0900

    編集

--- save_likes/resources/views/post/create.blade.php
+++ save_likes/resources/views/post/create.blade.php
@@ -25,11 +25,11 @@
     </header>
     <main>
         <div class="main-content">
-            <form action="/timeline" method="post">
+            <form action="{{ route('post.create') }}" method="post">
                 @csrf
                 <textarea name="content" class="text-input" placeholder="今何してる？"></textarea>
                 <div class="image-content">
-                    <img src="{{ asset('/storage/image/'.$post->user->avatar) }}" alt="" class="profile-picture">
+                    <img src="{{ asset('/storage/image/'. Auth::user()->avatar) }}" alt="" class="profile-picture">
                 </div>
             </form>
         </div>
